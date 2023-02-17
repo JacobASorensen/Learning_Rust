fb8dac65665088e982d15c21aeee31c6355102bd
esorensen33 <esorensen33@gatech.edu>
2023-02-17 16:43:52 -0500

    init first project

---
+++ hello-rust-erik/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/ubuntu/Github/Learning_Rust/hello-rust-erik/hello_cargo/target/debug/hello_cargo: /home/ubuntu/Github/Learning_Rust/hello-rust-erik/hello_cargo/src/main.rs
---
+++ hello-rust-erik/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/ubuntu/Github/Learning_Rust/hello-rust-erik/hello_cargo/target/release/hello_cargo: /home/ubuntu/Github/Learning_Rust/hello-rust-erik/hello_cargo/src/main.rs
