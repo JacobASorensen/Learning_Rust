03875389dd03ef91656eac0af4d1e12b61761b5f
Jinakaks <Jinakaks@gmail.com>
2023-02-18 19:13:05 -0500

    Finished Control Flow
    - Added 12 days of Christmas feature
    - Cleaned up program to compile without warnings
    i.e. destroyed all Camel_Case and substituted it for proper snake_case

--- control_flow_jacob/src/main.rs
+++ control_flow_jacob/src/main.rs
@@ -1,5 +1,5 @@
 use std::io;
-
+use std::io::Write;
 /*
 I will attempt to implement each of the following programs:
 
@@ -10,35 +10,25 @@ Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” taki
  */
 
 fn main() {
-    let mut doMainLoop = true;
+    let mut do_main_loop = true;
 
-        while doMainLoop {
+        while do_main_loop {
         println!("\n\nHello, what would you like to do? You have 4 choices:");
         println!("0: Convert between Fahrenheit and Celsius");
         println!("1: Generate the nth Fibonacci number");
         println!("2: Print lyrics to \"The Twelve Days of Christmas\"");
         println!("3: Exit the Program");
-
-        let mut choice = String::new();
         
         println!("\nPlease input your option; an integer from 0 to 3.");
-        io::stdin()
-            .read_line(&mut choice)
-            .expect("Failed to read line");
 
-        let choice: u32 = match choice.trim().parse()  {
-            Ok(num) => num,
-            Err(_) => {
-                println!("That was not an integer from 0 to 3.");
-                continue;
-            },
-        };
+
+        let choice: u32 = get_number::<u32>("That was not a valid integer".to_string());
 
         match choice {
             0 => do_temp_conversion(),
             1 => do_nth_fibonnacci(),
             2 => do_christmas_days(),
-            3 => doMainLoop = false,
+            3 => do_main_loop = false,
             4_u32..=u32::MAX => println!("That was not an integer from 0 to 3."),
         }
 
@@ -51,120 +41,125 @@ fn do_temp_conversion() {
     println!("1: Celsius to Fahrenheit");
     println!("\nPlease input your option; an integer from 0 to 1.");
     
-    let mut FtoC = true;
-    let mut hasFoundWay = false;
+    let mut f_to_c = true;
+    let mut has_found_way = false;
 
-        while !hasFoundWay {
+        while !has_found_way {
         
         println!("\nSo do you want to convert from Fahrenheit to Celsius or Celsius to Fahrenheit?");
         println!("0: Fahrenheit to Celsius");
         println!("1: Celsius to Fahrenheit");
         println!("\nPlease input your option; an integer from 0 to 1.");
-        let mut choice = String::new();
-        io::stdin()
-            .read_line(&mut choice)
-            .expect("Failed to read line");
-
-        let choice: u32 = match choice.trim().parse()  {
-            Ok(num) => num,
-            Err(_) => {
-                println!("That was not an integer from 0 to 1.");
-                continue;
-            },
-        };
+        
+        let choice: u32 = get_number::<u32>("That was not a valid integer".to_string());
 
         match choice {
-            0 => {FtoC = true;  hasFoundWay = true;},
-            1 => {FtoC = false; hasFoundWay = true;},
+            0 => {f_to_c = true;  has_found_way = true;},
+            1 => {f_to_c = false; has_found_way = true;},
             2_u32..=u32::MAX => println!("That was not an integer from 0 to 1."),
         };
     }
 
-    let mut tempName = "";
-    if FtoC {
-        tempName = "Fahrenheit";
+    let mut temperature_name;
+    if f_to_c {
+        temperature_name = "Fahrenheit";
     } else {
-        tempName = "Celsius";
+        temperature_name = "Celsius";
     }
 
-    println!("Enter a {tempName} temperature: ");
+    println!("Enter a {temperature_name} temperature: ");
 
-    let mut temperature = 0_f32;
+    let temperature = get_number::<f32>("That was not a valid number".to_string());
 
-    loop {
-        let mut input = String::new();
-        
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
+    let new_temperature;
 
-        let input: f32 = match input.trim().parse()  {
-            Ok(num) => num,
-            Err(_) => {
-                println!("That was not a valid number");
-                continue;
-            },
-        };
-        temperature = input;
-        break;
-    };
-
-    let mut newTemperature = 0_f32;
-
-    if FtoC {
-        newTemperature = (temperature - 32.0)/1.8;
-        tempName = "Celsius";
+    if f_to_c {
+        new_temperature = (temperature - 32.0)/1.8;
+        temperature_name = "Celsius";
     } else {
-        newTemperature = temperature*1.8 + 32.0;
-        tempName = "Fahrenheit";
+        new_temperature = temperature*1.8 + 32.0;
+        temperature_name = "Fahrenheit";
     }
-
     
-    println!("Your temperature in {tempName} is: {newTemperature}");
+    println!("Your temperature in {temperature_name} is: {new_temperature}");
+    please_press_enter();
 }
 
 fn do_nth_fibonnacci() {
-    
-    let SQUARE_ROOT_OF_FIVE = f32::powf(5.0, 0.5);
-    let INVERSE_SQRT_FIVE = 1.0/SQUARE_ROOT_OF_FIVE;
 
-    println!("\nSo do you want to convert from Fahrenheit to Celsius or Celsius to Fahrenheit?");
+    let square_root_of_five = f32::powf(5.0, 0.5);
+    let inverse_sqrt_of_five = 1.0/square_root_of_five;
+
     println!("\nPlease input the Fibonacci number that you want");
 
-    let mut fibonacciIndex = 0_u32;
-    let mut valid = false;
+    let fibonacci_index = get_number::<u32>("That was not a valid integer".to_string());
 
-    while !valid {
-        let mut choice = String::new();
-        io::stdin()
-            .read_line(&mut choice)
-            .expect("Failed to read line");
+    let mut left_num = 0.5*(1.0 + square_root_of_five);
+    let mut right_num = 0.5*(1.0 - square_root_of_five);
 
-        let choice: u32 = match choice.trim().parse()  {
-            Ok(num) => num,
-            Err(_) => {
-                println!("That was not an integer from 0 to 1.");
-                continue;
-            },
-        };
-        valid = true;
-        fibonacciIndex = choice;
-    }
+    left_num = f32::powf(left_num,fibonacci_index as f32);
+    right_num = f32::powf(right_num,fibonacci_index as f32);
 
-    let mut leftNum = 0.5*(1.0 + SQUARE_ROOT_OF_FIVE);
-    let mut rightNum = 0.5*(1.0 - SQUARE_ROOT_OF_FIVE);
-
-    leftNum = f32::powf(leftNum,fibonacciIndex as f32);
-    rightNum = f32::powf(rightNum,fibonacciIndex as f32);
-
-    let mut fibonacciNum = leftNum - rightNum;
-    fibonacciNum = INVERSE_SQRT_FIVE*fibonacciNum;
+    let mut fibonacci_num = left_num - right_num;
+    fibonacci_num = inverse_sqrt_of_five*fibonacci_num;
     
-    let fibonacciNum = fibonacciNum as u32;
-    println!("The {fibonacciIndex}th fibonacci number is {fibonacciNum}");
-
+    let fibonacci_num = fibonacci_num as u32;
+    println!("The {fibonacci_index}th fibonacci number is {fibonacci_num}");
+    please_press_enter();
 }
 
 fn do_christmas_days() {
+    let days = vec!["first","second","third","fourth","fifth","sixth",
+                    "seventh","eighth","ninth","tenth","eleventh","twelfth"];
+    let gifts = vec!["A partridge in a pear tree",
+                    "Two turtle doves",
+                    "Three french hens",
+                    "Four calling birds",
+                    "FIIIVVVEEEE GOLLLDDEEENNNN RRIIIIIINNNGGGSSSS",
+                    "Six geese a-laying",
+                    "Seven swans a-swimming",
+                    "Eight maids a-milking",
+                    "Nine ladies dancing",
+                    "Ten lords a-leaping",
+                    "Eleven pipers piping",
+                    "Twelve drummers drumming"];
 
+    let mut current_day = 0_usize;
+    for day in &days {
+        println!("On the {day} day of Christmas my true love gave to me:\n");
+        for i in 0_usize..=current_day {
+            println!("{}",gifts[i]);
+        }
+        current_day += 1;
+
+        please_press_enter();
+
+    }
 }
+
+fn please_press_enter() {
+    let mut choice = String::new();
+    print!("...");
+    io::stdout().flush().unwrap();
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read line");
+}
+
+fn get_number<A: std::str::FromStr>(error_message: String) -> A{
+    loop{
+            let mut choice = String::new();
+            io::stdin()
+                .read_line(&mut choice)
+                .expect("Failed to read line");
+
+            let choice: A = match choice.trim().parse()  {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("{}",error_message);
+                    continue;
+                },
+            };
+            return choice;
+        }
+    }
